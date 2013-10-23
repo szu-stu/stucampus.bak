d08cea1ec77026c85e64275c4440f143e4b03adf
doyoubi <doyoubihgx@gmail.com>
2013-10-23 17:20:11 +0800

    fix bug in manage of lecture&activity

--- stucampus/lecture/urls.py
+++ stucampus/lecture/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, patterns
-from stucampus.lecture.views import index, manage, submit
-from stucampus.lecture.views import delete, add_lecture
+from stucampus.lecture.views import index, manage, add_lecture
 
 
 urlpatterns = patterns(
@@ -8,6 +7,5 @@ urlpatterns = patterns(
     url(r'^$', index, name='index'),
     url(r'^manage/$', manage, name='manage'),
     url(r'^manage/(?P<page_num>\d+)/$', manage, name='manage'),
-    url(r'^submit/$', submit, name='submit'),
     url(r'^add_lecture/$', add_lecture, name='add_lecture'),
 )
