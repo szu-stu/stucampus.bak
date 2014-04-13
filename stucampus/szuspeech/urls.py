4f545f81ef52e06e70e373c408c383b25439dad5
shonenada <shonenada@gmail.com>
2014-04-13 19:31:03 +0800

    resize image when uploading

--- stucampus/szuspeech/urls.py
+++ stucampus/szuspeech/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-from stucampus.szuspeech.views import index,manage_list,del_resource, set_top,ModifyResorceView, AddResourceView
+from .views import index, manage_list, del_resource, set_top
+from .views import ModifyResorceView, AddResourceView
 
 
 urlpatterns = patterns('',
-	url(r'^$',index,name = 'index'),
+	url(r'^$', index, name='index'),
 	url(r'^manage/$', manage_list, name='manage_list'),
     url(r'^manage/upload/$', AddResourceView.as_view(), name='add'),
     url(r'^manage/edit/$', ModifyResorceView.as_view() , name='edit'),
